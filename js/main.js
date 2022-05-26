c305e0045c4c612dd76cf8fdb42c3c0de368196d
SohelTanbir <soheltanbir19@gmail.com>
2022-05-26 09:46:48 +0600

    add a function to show FAQ answer when user click to show ans on faq title

--- js/main.js
+++ js/main.js
@@ -9,4 +9,16 @@ function toggleIcon(){
     activeElement[1].classList.toggle("minus_icon");
 }
 
+// show FAQ Answer
+    $(".faq-header").click(showAnswer)
+    function showAnswer(){
+        // toggle ans
+        $(".faq-body").slideToggle("show");
+        
+        // some style
+        $(".faq-header").css({borderRadius:"5px 5px 0 0"})
+        $(".faq-body").css({borderRadius:"0 0 5px 5px"})
+    }
+
+
 })
